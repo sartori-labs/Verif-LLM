8a79c3ba10d27f231934040eb313cf6ad1e8f99c
Eldon Nelson <eldon@synopsys.com>
2024-10-20 11:39:14 -0500

    add in coverage to adder interface add in coverage report

--- designs/adder_8bit/adder_8bit_if.sv
+++ designs/adder_8bit/adder_8bit_if.sv
@@ -5,4 +5,33 @@ interface adder_8bit_if(input logic clk, input logic rst);
     logic cin;
     logic [7:0] sum;
     logic cout;
+	
+	// input covergroup
+	covergroup cg_inputs () @ (posedge clk && !rst);
+		a_stim: coverpoint a {
+			bins max = {255};
+			bins min = {0};
+			bins all[] = {[0:255]};
+		}
+		
+		b_stim: coverpoint b {
+			bins max = {255};
+			bins min = {0};
+			bins all[] = {[0:255]};
+		}
+		
+		cin_stim: coverpoint cin {
+			bins max = {1};
+			bins min = {0};
+			bins all[] = {[0:1]};
+		}
+		
+		full_stim: cross a_stim, b_stim, cin_stim {
+			bins all = binsof(a_stim.all) && binsof(b_stim.all) && binsof(cin_stim.all);
+		}
+		
+	endgroup
+	
+	cg_inputs cg_inputs_inst = new();
+	
 endinterface
